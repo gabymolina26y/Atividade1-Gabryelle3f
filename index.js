c6e041fc61b3bddee48b991e8da9c2c631078ea7
gabymolina26y <116806981+gabymolina26y@users.noreply.github.com>
2024-08-05 22:52:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const meuNome = "Gabryelle Molina";
+const idade = 16;
+const altura = 1.64;
+const massa = "60kg";
+const cidadeOndeMoro = "são josé dos pinhais";
+
+console.log(meuNome)
+console.log(idade)
+console.log(altura)
+console.log(massa)
+console.log()
+
+
+
+
+
+
+
