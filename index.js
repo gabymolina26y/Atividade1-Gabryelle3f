bef485a658ed228223d11efe535164ef28c6b1db
gabymolina26y <116806981+gabymolina26y@users.noreply.github.com>
2024-09-02 22:48:56 +0000

    02/09

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const altura = 1.64;
 const massa = "60kg";
 const cidadeOndeMoro = "são josé dos pinhais";
 
-console.log(meuNome)
-console.log(idade)
-console.log(altura)
-console.log(massa)
-console.log()
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidadeOndeMoro);
 
 
 
